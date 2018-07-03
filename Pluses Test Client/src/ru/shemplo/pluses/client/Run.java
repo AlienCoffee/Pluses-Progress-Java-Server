1b516f5d006f0db9015568bf24da22bc5f7eb542
Shemplo <shemplo@outlook.com>
2018-07-03 21:18:03 +0300

    Message receiving changed

--- Pluses Test Client/src/ru/shemplo/pluses/client/Run.java
+++ Pluses Test Client/src/ru/shemplo/pluses/client/Run.java
@@ -1,6 +1,7 @@
 package ru.shemplo.pluses.client;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -24,14 +25,27 @@ public class Run {
 		Reader r = new InputStreamReader (System.in, StandardCharsets.UTF_8);
 		BufferedReader br = new BufferedReader (r);
 		
-		ObjectOutputStream bos = new ObjectOutputStream (os);
-		
 		String line = null;
 		while ((line = br.readLine ()) != null) {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream ();
+			ObjectOutputStream oos = new ObjectOutputStream (baos);
+			
 			MessageDirection dir = MessageDirection.CLIENT_TO_SERVER;
 			JavaMessage message = new JavaMessage (dir, line);
-			bos.writeObject (message);
-			bos.flush ();
+			oos.writeObject (message);
+			oos.flush ();
+			
+			byte [] data = baos.toByteArray ();
+			System.out.println (data.length);
+			byte [] length = {
+				(byte) (data.length >> 24 & 0xff),
+				(byte) (data.length >> 16 & 0xff),
+				(byte) (data.length >> 8 & 0xff),
+				(byte) (data.length & 0xff)
+			};
+			os.write (length);
+			os.write (data);
+			os.flush ();
 		}
 		
 		socket.close ();
